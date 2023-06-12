f3dcfe7b4e33c4a8d03941699b953b7822749e55
anilas5235 <niklas-borchers@outlook.de>
2023-06-12 13:22:42 +0200

    Level Preview

---
+++ Assets/Project/Scripts/UIScripts/PlayPreviewWindow.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Project.Scripts.UIScripts
+{
+    public class PlayPreviewWindow : MonoBehaviour
+    {
+        [SerializeField] private Image star1, star2;
+        [SerializeField] private Slider progressBar;
+
+    }
+}
